701d56a85ed1c7299f28587fb8a80c0e18ddaded
imjoo0 <im_joo0@naver.com>
2023-09-21 04:40:08 +0000

    tt

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import "./App.css";
 function App() {
   return (
     <Routes>
-      <Route path={`${process.env.PUBLIC_URL}/`} element={<LoginForm />} />
-      <Route path={`${process.env.PUBLIC_URL}/about`} exact={true} element={<LoginForm />} />
+      <Route path={`/LMS_front/`} element={<LoginForm />} />
+      <Route path={`/LMS_front/about`} exact={true} element={<LoginForm />} />
     </Routes>
   );
 }
