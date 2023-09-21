bb5f27c22c15e21f117aeea785491575971307ac
imjoo0 <im_joo0@naver.com>
2023-09-21 01:26:11 +0000

    deploy plz

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // BrowserRouter를 추가해주세요.
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 // import UserProfile from './components/UserProfile';
 // import PrivateRoute from './routes/PrivateRoute';
@@ -17,13 +17,8 @@ function App() {
   };
 
   return (
-    <Router> 
-      <Routes>
-        <Route
-          path={`${process.env.PUBLIC_URL}/`} // 이 부분에서 path를 설정합니다.
-          element={<LoginForm />} // 해당 path에 매칭될 컴포넌트를 지정합니다.
-        />
-      </Routes>
+    <Router basename={process.env.PUBLIC_URL}> 
+      <LoginForm />
     </Router> 
   );
 }
